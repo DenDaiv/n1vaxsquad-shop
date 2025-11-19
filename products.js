be873d7bb8d9372692ebe72d86da97a05d316796
DenDaiv <prostoakk448@gmail.com>
2025-11-19 20:39:31 +0200

    Update products.js

--- products.js
+++ products.js
@@ -1,37 +1,38 @@
+/* Список товаров — изменяй/добавляй здесь */
 const products = [
   {
     id: 1,
-    name: "Игровой ноутбук X-Game",
-    desc: "RTX, SSD, быстрый экран — идеально для игр.",
-    price: 39999,
-    imageText: "Фото ноутбука"
+    name: "Ноутбук игровой Gigabyte Gaming A16 (CMHI2UA894SD) Black Steel",
+    desc: "GeForce RTX 4050, 16GB RAM, 1TB NVMe SSD — для комфортного гейминга и стрима.",
+    price: 44 999,
+    imageUrl: "https://scdn.comfy.ua/89fc351a-22e7-41ee-8321-f8a9356ca351/https://cdn.comfy.ua/media/catalog/product/c/w/cwhi3ua864sd-560165-2167761_1_.jpg/w_600" // можно указать путь 'img/laptop.png' или пусто — будет заглушка
   },
   {
     id: 2,
-    name: "Смартфон Pro Max 12",
-    desc: "Камера, батарея, мощность.",
-    price: 24499,
-    imageText: "Фото смартфона"
+    name: "Смартфон Apple iPhone 13 128Gb Midnight",
+    desc: "6.7\" OLED, 5000mAh, камера 108MP — выбор для фото и игр.",
+    price: 22 999,
+    imageUrl: "https://scdn.comfy.ua/89fc351a-22e7-41ee-8321-f8a9356ca351/https://cdn.comfy.ua/media/catalog/product/i/p/iphone_13_q421_midnight_pdp_image_position-1a__ww-ru_1_.jpg/w_600"
   },
   {
     id: 3,
-    name: "Наушники SoundX",
-    desc: "Шумоподавление, 30 ч автономности.",
-    price: 3299,
-    imageText: "Фото наушников"
+    name: "TWS Apple AirPods Pro 3",
+    desc: "Беспроводные, шумоподавление, до 30 часов работы.",
+    price: 14 099,
+    imageUrl: "https://scdn.comfy.ua/89fc351a-22e7-41ee-8321-f8a9356ca351/https://cdn.comfy.ua/media/catalog/product/a/i/airpods-pro-3-hero-select-202509.jpg/w_600"
   },
   {
     id: 4,
-    name: "Монитор UltraView 27\"",
-    desc: "144 Гц, IPS, идеален для игр.",
-    price: 9999,
-    imageText: "Фото монитора"
+    name: "Монитор Samsung Odyssey G5\"",
+    desc: "IPS, 180 Гц, 1 мс — отличный выбор для игр и работы.",
+    price: 10 899,
+    imageUrl: "https://scdn.comfy.ua/89fc351a-22e7-41ee-8321-f8a9356ca351/https://cdn.comfy.ua/media/catalog/product/l/s/ls27dg500eixci.jpg/w_600"
   },
   {
-    id: 5,
-    name: "Колонка ThunderBass X7",
-    desc: "Мощный бас, 12 часов работы.",
-    price: 1899,
-    imageText: "Фото колонки"
+    id: 5\,
+    name: "Портативна акустика JBL GO 4 Black",
+    desc: "Мощный бас, Bluetooth, USB Type-C, 12 часов автономии.",
+    price: 1 499,
+    imageUrl: "https://scdn.comfy.ua/89fc351a-22e7-41ee-8321-f8a9356ca351/https://cdn.comfy.ua/media/catalog/product/6/6/66103a64ae9d1.jpg/w_600"
   }
 ];
