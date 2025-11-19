892cbcccb965e834bec62584b0543f29a7da5201
DenDaiv <prostoakk448@gmail.com>
2025-11-19 20:14:01 +0200

    add product.js

---
+++ products.js
@@ -0,0 +1,37 @@
+const products = [
+  {
+    id: 1,
+    name: "Игровой ноутбук X-Game",
+    desc: "RTX, SSD, быстрый экран — идеально для игр.",
+    price: 39999,
+    imageText: "Фото ноутбука"
+  },
+  {
+    id: 2,
+    name: "Смартфон Pro Max 12",
+    desc: "Камера, батарея, мощность.",
+    price: 24499,
+    imageText: "Фото смартфона"
+  },
+  {
+    id: 3,
+    name: "Наушники SoundX",
+    desc: "Шумоподавление, 30 ч автономности.",
+    price: 3299,
+    imageText: "Фото наушников"
+  },
+  {
+    id: 4,
+    name: "Монитор UltraView 27\"",
+    desc: "144 Гц, IPS, идеален для игр.",
+    price: 9999,
+    imageText: "Фото монитора"
+  },
+  {
+    id: 5,
+    name: "Колонка ThunderBass X7",
+    desc: "Мощный бас, 12 часов работы.",
+    price: 1899,
+    imageText: "Фото колонки"
+  }
+];
