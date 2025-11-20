59054be911715f3a2530b4b21f53a73811ad672d
DenDaiv <prostoakk448@gmail.com>
2025-11-20 10:27:50 +0200

    Fix product prices and image URLs formatting

--- products.js
+++ products.js
@@ -4,35 +4,35 @@ const products = [
     id: 1,
     name: "Ноутбук игровой Gigabyte Gaming A16 (CMHI2UA894SD) Black Steel",
     desc: "GeForce RTX 4050, 16GB RAM, 1TB NVMe SSD — для комфортного гейминга и стрима.",
-    price: 44 999,
-    imageUrl: "https://scdn.comfy.ua/89fc351a-22e7-41ee-8321-f8a9356ca351/https://cdn.comfy.ua/media/catalog/product/c/w/cwhi3ua864sd-560165-2167761_1_.jpg/w_600" // можно указать путь 'img/laptop.png' или пусто — будет заглушка
+    price: 44999,
+    imageUrl: "https://cdn.comfy.ua/media/catalog/product/c/w/cwhi3ua864sd-560165-2167761_1_.jpg"
   },
   {
     id: 2,
     name: "Смартфон Apple iPhone 13 128Gb Midnight",
     desc: "6.7\" OLED, 5000mAh, камера 108MP — выбор для фото и игр.",
-    price: 22 999,
-    imageUrl: "https://scdn.comfy.ua/89fc351a-22e7-41ee-8321-f8a9356ca351/https://cdn.comfy.ua/media/catalog/product/i/p/iphone_13_q421_midnight_pdp_image_position-1a__ww-ru_1_.jpg/w_600"
+    price: 22999,
+    imageUrl: "https://cdn.comfy.ua/media/catalog/product/i/p/iphone_13_q421_midnight_pdp_image_position-1a__ww-ru_1_.jpg"
   },
   {
     id: 3,
     name: "TWS Apple AirPods Pro 3",
     desc: "Беспроводные, шумоподавление, до 30 часов работы.",
-    price: 14 099,
-    imageUrl: "https://scdn.comfy.ua/89fc351a-22e7-41ee-8321-f8a9356ca351/https://cdn.comfy.ua/media/catalog/product/a/i/airpods-pro-3-hero-select-202509.jpg/w_600"
+    price: 14099,
+    imageUrl: "https://cdn.comfy.ua/media/catalog/product/a/i/airpods-pro-3-hero-select-202509.jpg"
   },
   {
     id: 4,
-    name: "Монитор Samsung Odyssey G5\"",
+    name: "Монитор Samsung Odyssey G5",
     desc: "IPS, 180 Гц, 1 мс — отличный выбор для игр и работы.",
-    price: 10 899,
-    imageUrl: "https://scdn.comfy.ua/89fc351a-22e7-41ee-8321-f8a9356ca351/https://cdn.comfy.ua/media/catalog/product/l/s/ls27dg500eixci.jpg/w_600"
+    price: 10899,
+    imageUrl: "https://cdn.comfy.ua/media/catalog/product/l/s/ls27dg500eixci.jpg"
   },
   {
     id: 5,
-    name: "Портативна акустика JBL GO 4 Black",
+    name: "Портативная акустика JBL GO 4 Black",
     desc: "Мощный бас, Bluetooth, USB Type-C, 12 часов автономии.",
-    price: 1 499,
-    imageUrl: "https://scdn.comfy.ua/89fc351a-22e7-41ee-8321-f8a9356ca351/https://cdn.comfy.ua/media/catalog/product/6/6/66103a64ae9d1.jpg/w_600"
+    price: 1499,
+    imageUrl: "https://cdn.comfy.ua/media/catalog/product/6/6/66103a64ae9d1.jpg"
   }
 ];
